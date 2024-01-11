9ac82183cc7652943324b9bacc7e988fc85528ef
Diurno <Diurno@DESKTOP-A2FPPFB>
2024-01-11 12:49:39 +0100

    prueba

---
+++ Proyecto1/src/paquete1/Principal.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		System.out.println("Ejemplo de control de version de Git");
+		System.out.println("Segudno ejemplo de control de versiones con Git");
+	}
+
+}
